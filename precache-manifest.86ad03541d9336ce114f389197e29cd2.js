3787c30c96d1f6d1e7d32a7011d4ba88708177f5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-24 09:23:45 +0000

    Deploy Outc4sted/PokePather to github.com/Outc4sted/PokePather.git:gh-pages

---
+++ precache-manifest.86ad03541d9336ce114f389197e29cd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3876c7c1c06bd66e8c70",
+    "url": "/PokePather/static/css/main.9e292124.chunk.css"
+  },
+  {
+    "revision": "3876c7c1c06bd66e8c70",
+    "url": "/PokePather/static/js/main.3876c7c1.chunk.js"
+  },
+  {
+    "revision": "ba4e2a06c84e97ce7a54",
+    "url": "/PokePather/static/js/runtime~main.ba4e2a06.js"
+  },
+  {
+    "revision": "b098a487139f5636a522",
+    "url": "/PokePather/static/css/2.d281382e.chunk.css"
+  },
+  {
+    "revision": "b098a487139f5636a522",
+    "url": "/PokePather/static/js/2.b098a487.chunk.js"
+  },
+  {
+    "revision": "d43dda34e673b6fb7efef585cb12833e",
+    "url": "/PokePather/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e236aaf07bd37069a47e71d546675155.js"
+  "/PokePather/precache-manifest.86ad03541d9336ce114f389197e29cd2.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/PokePather/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
